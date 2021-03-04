2d223fedf0031cd5177e3c73c8142bf954cfcddc
Caio Barutti <caiohbfurtado@gmail.com>
2021-03-03 21:21:36 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://caieras.pika">Caieras, pika!</a>
         </h1>
 
         <p className={styles.description}>
